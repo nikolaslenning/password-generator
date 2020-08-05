f48f8853f91b7c0731e95136fd36a07b0f50d5b2
Nikolas Lenning <nikolaslenning@gmail.com>
2020-08-05 15:35:46 -0600

    made for loop inside generatePassword function

--- Develop/assets/scripts/script.js
+++ Develop/assets/scripts/script.js
@@ -3,18 +3,18 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword () { 
   //Define Character variables
-var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ;
-var lowercase = "abcdefghijklmnopqrstuvwxyz" ;
-var numeric = "1234567890" ;
-var special = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~" ;
-// special characters list - https://owasp.org/www-community/password-special-characters
-var charactersToUse = ''
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ;
+  var lowercase = "abcdefghijklmnopqrstuvwxyz" ;
+  var numeric = "1234567890" ;
+  var special = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~" ;
+  // special characters list - https://owasp.org/www-community/password-special-characters
+  var charactersToUse = ''
 
   var passwordLength = prompt("Please choose a character length between 8 and 128");
   console.log('user selected a length of', passwordLength)
   // generate numbers, letters, symbols
   if(passwordLength >128 || passwordLength <8) {
-    alert( "nope")
+    alert( "Invalid Length - Please choose a Character length between 8 and 128")
     generatePassword();
   
   }
@@ -30,10 +30,7 @@ var charactersToUse = ''
  
   var useSpecial = confirm("Use special?");
   console.log('Should I use Special characters?', useSpecial) 
-
-  // if(useUpperCase) {
-  //   charactersToUse += uppercase
-  // }
+ 
   if(useUpperCase) {
     charactersToUse = charactersToUse.concat(uppercase) ;
   }
@@ -46,30 +43,27 @@ var charactersToUse = ''
   if(useSpecial) {
     charactersToUse = charactersToUse.concat(special)
   }
-  console.log(charactersToUse)
-
+  // For-loop  generates random index of characterToUse and assigns value to pickChar. loops through number of times equal to password length
+  for( var i = 0 ; i < passwordLength; i++) {
+    var pickChar = charactersToUse[Math.floor(Math.random() * charactersToUse.length)] ;
+    console.log(pickChar);
+    // password.push(pickChar);
+  }
 }
 
- //add characters selected to password based on selected length that loops. at each step adds character from characters to use at random index. use for loop
-
-
-
-
+ //add characters selected to password based on selected length that loops. at each step adds character from charactersToUse at random index. use for-loop
 
 
 // Write password to the #password input
 function writePassword() {
-  console.log("writePassword is running");
+  // console.log("writePassword is running");
   var password = generatePassword();
-
-  console.log(password)
+  // console.log(password)
   var passwordText = document.querySelector("#password");
-
-  console.log(passwordText);
+  // console.log(passwordText);
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); 
